ed1ed351f8321b327f63d2e01338a423a5cb55db
He who is brandon gulla <brandon@brandongulla.com>
2018-11-28 11:17:18 -0500

    deleting bad unicode

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-port = 8443 # Note, eventhough we are declaring this here, OpenShift hardcodes port 8443, this cannot be overridden ¯\_(ツ)_/¯  .
+port = 8443 
 certfile = '/secret/cert/python-tls.crt'
 keyfile = '/secret/cert/python-tls.key'
